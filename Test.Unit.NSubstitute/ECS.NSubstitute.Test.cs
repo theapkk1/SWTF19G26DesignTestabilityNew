d34ad531c352e6a3fedc94ea6da884569c12f611
Celia <au582514@post.au.dk>
2019-02-19 13:55:44 +0100

    tilføjet en test via NSubstitute

---
+++ Test.Unit.NSubstitute/ECS.NSubstitute.Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewECS.Legacy;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Test.Unit.NSubstitute
+{
+    [TestFixture]
+    public class Class1
+    {
+        private ECS _uut;
+        private ITempSensor _tempSensor;
+        private IHeater _heater;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _heater = Substitute.For<IHeater>();
+            _tempSensor = Substitute.For<ITempSensor>();
+
+            _uut = new ECS(_tempSensor, _heater, 20);
+        }
+
+        [Test]
+        public void Regulate_LastTempCorrect()
+        {
+
+            int A = _tempSensor.GetTemp(); // Act
+            _uut.GetCurTemp();
+            Assert.That(_uut.GetCurTemp(), Is.EqualTo(A));
+        }
+    }
+}
+
+
+
